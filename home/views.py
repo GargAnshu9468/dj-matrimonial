e0d79d6910b4e643943064d459ebea5e31a6eaf8
shani <shanikumar0045@gmail.com>
2023-03-09 00:29:55 +0530

    adding new templates

--- home/views.py
+++ home/views.py
@@ -4,4 +4,16 @@ from django.shortcuts import render
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "home/index.html")
+        return render(request, "components/home.html")
+
+class About(View):
+    def get(self,request):
+        return render(request,'components/about.html')
+    
+class Story(View):
+    def get(self,request):
+        return render(request,'components/story.html')
+    
+class Gallery(View):
+    def get(self,request):
+        return render(request,'components/gallery.html')
