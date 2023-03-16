f2558ced26936fb4dc3e198718f853bd399902a4
awanishkr <awanishkr@tradeindia.com>
2023-03-16 14:41:12 +0530

    logout credentials

--- login/urls.py
+++ login/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.SignIn.as_view(), name='signin'),
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('logout/', views.log_out, name='logout'),
+    path('change-password/', views.change_password, name='change_password'),
 ]
