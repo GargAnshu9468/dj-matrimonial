391aed80557be29f02aade7071182a697611d7c2
awanishkr <awanishkr@tradeindia.com>
2023-03-16 17:53:58 +0530

    implementing profile icon

--- home/views.py
+++ home/views.py
@@ -1,37 +1,40 @@
 from django.views.generic import View
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
+@method_decorator(login_required(login_url='/'), name='dispatch')
 class Home(View):
     def get(self, request):
         return render(request, 'components/home.html')
 
-
+@method_decorator(login_required(login_url='/'), name='dispatch')
 class About(View):
     def get(self, request):
         return render(request, 'components/about.html')
 
-
+@method_decorator(login_required(login_url='/'), name='dispatch')
 class Story(View):
     def get(self, request):
         return render(request, 'components/story.html')
 
-
+@method_decorator(login_required(login_url='/'), name='dispatch')
 class Gallery(View):
     def get(self, request):
         return render(request, 'components/gallery.html')
 
-
+@method_decorator(login_required(login_url='/'), name='dispatch')
 class Family(View):
     def get(self, request):
         return render(request, 'components/friends-family.html')
 
-
+@method_decorator(login_required(login_url='/'), name='dispatch')
 class Event(View):
     def get(self, request):
         return render(request, 'components/event.html')
 
-
+@method_decorator(login_required(login_url='/'), name='dispatch')
 class RSVP(View):
     def get(self, request):
         return render(request, 'components/rsvp.html')
