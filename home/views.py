259d1d3fcf9d62f69d004e9262f2eebb8aca9f74
awanishkr <awanishkr@tradeindia.com>
2023-03-18 16:06:03 +0530

    implement geolocation

--- home/views.py
+++ home/views.py
@@ -2,7 +2,9 @@ from django.views.generic import View
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-
+from django.views.decorators.csrf import csrf_exempt
+from models.home.LatitudeLongitudeCities import LatitudeLongitudeCity
+from django.http import HttpResponse,JsonResponse
 
 @method_decorator(login_required(login_url='/'), name='dispatch')
 class Home(View):
@@ -38,3 +40,24 @@ class Event(View):
 class RSVP(View):
     def get(self, request):
         return render(request, 'components/rsvp.html')
+
+@csrf_exempt
+def get_current_city(request):
+    cities = LatitudeLongitudeCity().cities
+
+    latitude = float(request.POST.get('latitude', 0))
+    longitude = float(request.POST.get('longitude', 0))
+
+    if not latitude and not longitude:
+        return JsonResponse({'city': ''})
+
+    cities_dict = {}
+
+    for city, _, latitude_float, longitude_float in cities:
+        rank = ((latitude_float - latitude) ** 2 + (longitude_float - longitude) ** 2) ** 0.5
+        cities_dict[rank] = {'city': city}
+
+    city = cities_dict[sorted(cities_dict)[0]].get('city')
+    return JsonResponse({'city': city})
+
+
